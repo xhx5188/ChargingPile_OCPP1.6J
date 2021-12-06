f37bc55962f49943d4796f27dc70541061963d05
pj635 <2559394890@qq.com>
2021-12-06 17:59:59 +0800

    fix

--- connector/connector.py
+++ connector/connector.py
@@ -1,77 +1,68 @@
-import logging
-import time
 import socket
 import yaml
-# from connector.serial import Serial
-#
-# class Connector():
-#     serial = None
-#     def __init__(self):
-#         yaml_file = "../../config.yaml"
-#         with open(yaml_file, 'r') as f:
-#             cfg = yaml.safe_load(f)
-#             connector = cfg['connector']
-#             Connector.serial = Serial(connector['com'], connector['bps'])
-#
-#     @classmethod
-#     def slot(self):
-#         Connector.serial.send_hex_data("a00101a2")
-#
-#     @classmethod
-#     def unslot(self):
-#         Connector.serial.send_hex_data("a00100a1")
-#
-#
-# def test_11():
-#     Connector()
-#     Connector.unslot()
-#
-# def test_22():
-#     Connector()
-#     Connector.slot()
 
 class Connector():
     @classmethod
-    def slot(cls):
+    def slot(cls, path="../../config.yaml"):
+        with open(path, 'r') as f:
+            cfg = yaml.safe_load(f)
+            ip = cfg['connector']['STAIP']
+            port = cfg['connector']['port']
+
         s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-        s.connect(("10.10.41.44", 8080))
+        s.connect((ip, port))
         data = bytes.fromhex("a00101a2")
         s.send(data)
         s.close()
 
     @classmethod
-    def unslot(cls):
+    def unslot(cls, path="../../config.yaml"):
+        with open(path, 'r') as f:
+            cfg = yaml.safe_load(f)
+            ip = cfg['connector']['STAIP']
+            port = cfg['connector']['port']
+
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("10.10.41.44", 8080))
+        s.connect((ip, port))
         data = bytes.fromhex("a00100a1")
         s.send(data)
         s.close()
 
     @classmethod
-    def unelectricity(cls):
+    def unelectricity(cls, path="../../config.yaml"):
+        with open(path, 'r') as f:
+            cfg = yaml.safe_load(f)
+            ip = cfg['connector']['STAIP']
+            port = cfg['connector']['port']
+
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("10.10.41.44", 8080))
+        s.connect((ip, port))
         data = bytes.fromhex("a00401a5")
         s.send(data)
         s.close()
 
     @classmethod
-    def electricity(cls):
+    def electricity(cls, path="../../config.yaml"):
+        with open(path, 'r') as f:
+            cfg = yaml.safe_load(f)
+            ip = cfg['connector']['STAIP']
+            port = cfg['connector']['port']
+
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("10.10.41.44", 8080))
+        s.connect((ip, port))
         data = bytes.fromhex("a00400a4")
         s.send(data)
         s.close()
 
 def test_unelectricity():
-    Connector.unelectricity()
+    Connector.unelectricity("../config.yaml")
 
 def test_electricity():
-    Connector.electricity()
+    Connector.electricity("../config.yaml")
 
 def test_slot():
-    Connector.slot()
+    Connector.slot("../config.yaml")
 
 def test_unslot():
-    Connector.unslot()
+    Connector.unslot("../config.yaml")
