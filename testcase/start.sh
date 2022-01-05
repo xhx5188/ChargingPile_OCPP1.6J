823781ff2905dc42293313a36569e966beec9d25
pj635 <2559394890@qq.com>
2022-01-05 17:58:32 +0800

    update

--- testcase/start.sh
+++ testcase/start.sh
@@ -6,7 +6,7 @@ rm -rf report
 reprot_path="../report"
 
 array=(01 02 03 04 05 06 07 08 09 10
-        11 12 13 14 15 16 17 18
+        11 12 13 14 15 16 17 18 19
         20 21 23 25)
 
 echo ""
@@ -18,11 +18,10 @@ echo ""
 #执行需要刷卡用例
 for e in ${array[@]}; do
   echo "swipe card testcase suite_$e"
-  echo ""
-  echo ""
   cd test_2_${e}*
-  pytest test*.py --alluredir ${reprot_path} -m "need_swipe_card"
+#  pytest test*.py --alluredir ${reprot_path} -m "need_swipe_card"
   cd ..
+
 done
 
 echo ""
@@ -41,8 +40,6 @@ echo ""
 #执行不需要刷卡用例
 for e in ${array[@]}; do
   echo "no swipe card testcase suite_$e"
-  echo ""
-  echo ""
   cd test_2_${e}*
   pytest test*.py --alluredir ${reprot_path} -m "not need_swipe_card"
   cd ..
--- testcase/test_2_19_local_authorization_list/test_send_local_authorization_list.py
+++ testcase/test_2_19_local_authorization_list/test_send_local_authorization_list.py
@@ -5,9 +5,10 @@ from ocpp.v16.enums import RegistrationStatus
 from server import service
 import allure
 
-@allure.feature("test_get_local_list_version_empty")
+
+@allure.feature("test_send_local_authorization_list1")
 @pytest.mark.asyncio
-async def test_get_local_list_version_empty(event_loop):
+async def test_send_local_authorization_list1(event_loop):
     # 改变配置信息"LocalAuthListEnabled"
     response = await service.changeConfiguration(event_loop, key="LocalAuthListEnabled", value="true")
     assert response[0].status == "Accepted"
@@ -50,3 +51,99 @@ async def test_get_local_list_version_empty(event_loop):
     response = await service.getLocalListVersion(event_loop)
     logging.info(response)
     assert response[0].list_version == data.get("listVersion")
+
+
+@allure.feature("test_send_local_authorization_list2")
+@pytest.mark.asyncio
+async def test_send_local_authorization_list2(event_loop):
+    # 改变配置信息"LocalAuthListEnabled"
+    response = await service.changeConfiguration(event_loop, key="LocalAuthListEnabled", value="false")
+    assert response[0].status == "Accepted"
+
+    # 发送本地列表给桩 updateType=full
+    with open("schema/SendLocalList1.json", 'r') as f:
+        data = json.load(f)
+    response = await service.sendLocalList(event_loop, list_version=data.get("listVersion"),
+                                           update_type=data.get("updateType"),
+                                           local_authorization_list=data.get("localAuthorizationList"))
+    assert response[0].status == "NotSupported"
+
+
+@allure.feature("test_send_local_authorization_list3")
+@pytest.mark.asyncio
+async def test_send_local_authorization_list3(event_loop):
+    # 改变配置信息"LocalAuthListEnabled"
+    response = await service.changeConfiguration(event_loop, key="LocalAuthListEnabled", value="true")
+    assert response[0].status == "Accepted"
+
+    # 发送本地列表给桩 updateType=full
+    with open("schema/SendLocalList3_1.json", 'r') as f:
+        data = json.load(f)
+    response = await service.sendLocalList(event_loop, list_version=data.get("listVersion"),
+                                           update_type=data.get("updateType"),
+                                           local_authorization_list=data.get("localAuthorizationList"))
+    assert response[0].status == RegistrationStatus.accepted
+
+    # 获取桩的本地列表版本
+    response = await service.getLocalListVersion(event_loop)
+    logging.info(response)
+    assert response[0].list_version == data.get("listVersion")
+
+
+    # 发送本地列表给桩 updateType=full
+    with open("schema/SendLocalList3_2.json", 'r') as f:
+        data = json.load(f)
+    response = await service.sendLocalList(event_loop, list_version=data.get("listVersion"),
+                                           update_type=data.get("updateType"),
+                                           local_authorization_list=data.get("localAuthorizationList"))
+    assert response[0].status == RegistrationStatus.accepted
+
+    # 获取桩的本地列表版本
+    response = await service.getLocalListVersion(event_loop)
+    logging.info(response)
+    assert response[0].list_version == data.get("listVersion")
+    version = data.get("listVersion")
+
+    # 发送本地列表给桩 updateType=Differential
+    with open("schema/SendLocalList3_3.json", 'r') as f:
+        data = json.load(f)
+    response = await service.sendLocalList(event_loop, list_version=data.get("listVersion"),
+                                           update_type=data.get("updateType"),
+                                           local_authorization_list=data.get("localAuthorizationList"))
+    assert response[0].status == "VersionMismatch"
+
+    # 获取桩的本地列表版本
+    response = await service.getLocalListVersion(event_loop)
+    logging.info(response)
+    assert response[0].list_version == version
+
+
+    # 清空本地列表
+    with open("./schema/clearLocalList.json", 'r') as f:
+        data = json.load(f)
+    response = await service.sendLocalList(event_loop, list_version=data.get("listVersion"),
+                                           update_type=data.get("updateType"),
+                                           local_authorization_list=data.get("localAuthorizationList"))
+    assert response[0].status == RegistrationStatus.accepted
+
+    # 获取本地列表版本
+    response = await service.getLocalListVersion(event_loop)
+    logging.info(response)
+    assert response[0].list_version == data.get("listVersion")
+
+
+@pytest.mark.skip("需要模拟故障")
+@allure.feature("test_send_local_authorization_list4")
+@pytest.mark.asyncio
+async def test_send_local_authorization_list4(event_loop):
+    # 改变配置信息"LocalAuthListEnabled"
+    response = await service.changeConfiguration(event_loop, key="LocalAuthListEnabled", value="true")
+    assert response[0].status == "Accepted"
+
+    # 发送本地列表给桩 updateType=full
+    with open("schema/SendLocalList1.json", 'r') as f:
+        data = json.load(f)
+    response = await service.sendLocalList(event_loop, list_version=data.get("listVersion"),
+                                           update_type=data.get("updateType"),
+                                           local_authorization_list=data.get("localAuthorizationList"))
+    assert response[0].status == "Failed"
