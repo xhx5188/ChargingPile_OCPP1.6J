0540e044b9549ce87da28535ebcf8818e3a2c8b3
pj635 <2559394890@qq.com>
2022-02-09 21:44:10 +0800

    修改用例

--- testcase/test_2_07_remote_start_stop_transaction/test_remote_start_stop_transaction.py
+++ testcase/test_2_07_remote_start_stop_transaction/test_remote_start_stop_transaction.py
@@ -12,7 +12,6 @@ import allure
 @allure.feature("test_remote_start_transaction_cable_plugged")
 @pytest.mark.asyncio
 async def test_remote_start_transaction_cable_plugged(event_loop):
-    logging.info("查看时间")
     # 获取配置信息"AuthorizeRemoteTxRequests"
     result = await service.getConfiguration(event_loop, ["AuthorizeRemoteTxRequests"])
     logging.info(result)
@@ -27,7 +26,7 @@ async def test_remote_start_transaction_cable_plugged(event_loop):
 
     # 远程启动充电
     clearTriggerMessage()
-    with open("schema/RemoteStartTransaction.json", 'r') as f:
+    with open("../schema/RemoteStartTransaction.json", 'r') as f:
         data = json.load(f)
     response = await service.remoteStartTransaction(event_loop, id_tag=data.get('idTag'),
                                                     connector_id=data.get('connectorId'),
@@ -52,7 +51,7 @@ async def test_remote_start_transaction_cable_plugged(event_loop):
 
     clearTriggerMessage()
     # 结束远程充电
-    response = await service.remoteStopTransaction(event_loop, data['chargingProfile']['transactionId'])
+    response = await service.remoteStopTransaction(event_loop, Value.transactionId_1)
     assert response[0].status == RegistrationStatus.accepted
 
     # 等待本地开始充电
@@ -94,7 +93,7 @@ async def test_remote_start_transaction(event_loop):
 
     # 远程启动充电
     clearTriggerMessage()
-    with open("schema/RemoteStartTransaction.json", 'r') as f:
+    with open("../schema/RemoteStartTransaction.json", 'r') as f:
         data = json.load(f)
     response = await service.remoteStartTransaction(event_loop, id_tag=data.get('idTag'),
                                                     connector_id=data.get('connectorId'),
@@ -115,7 +114,7 @@ async def test_remote_start_transaction(event_loop):
 
     clearTriggerMessage()
     # 结束远程充电
-    response = await service.remoteStopTransaction(event_loop, data['chargingProfile']['transactionId'])
+    response = await service.remoteStopTransaction(event_loop, Value.transactionId_1)
     assert response[0].status == RegistrationStatus.accepted
 
     # 等待本地开始充电
@@ -149,7 +148,7 @@ async def test_remote_start_transaction_time_out(event_loop):
 
     # 远程启动充电
     clearTriggerMessage()
-    with open("schema/RemoteStartTransaction.json", 'r') as f:
+    with open("../schema/RemoteStartTransaction.json", 'r') as f:
         data = json.load(f)
     response = await service.remoteStartTransaction(event_loop, id_tag=data.get('idTag'),
                                                     connector_id=data.get('connectorId'),
@@ -191,7 +190,7 @@ async def test_remote_stop_transaction(event_loop):
 
     # 远程启动充电
     clearTriggerMessage()
-    with open("schema/RemoteStartTransaction.json", 'r') as f:
+    with open("../schema/RemoteStartTransaction.json", 'r') as f:
         data = json.load(f)
     response = await service.remoteStartTransaction(event_loop, id_tag=data.get('idTag'),
                                                     connector_id=data.get('connectorId'),
@@ -207,7 +206,7 @@ async def test_remote_stop_transaction(event_loop):
     assert status == "Charging"
 
     # 结束远程充电
-    response = await service.remoteStopTransaction(event_loop, data['chargingProfile']['transactionId'])
+    response = await service.remoteStopTransaction(event_loop, Value.transactionId_1)
     assert response[0].status == RegistrationStatus.accepted
     status = await waitConnectorStatus(1, "Preparing")
     assert status == "Preparing"
@@ -223,51 +222,3 @@ async def test_remote_stop_transaction(event_loop):
 
 
 
-
-
-@allure.feature("test_remote_start_transaction")
-@pytest.mark.asyncio
-async def test_remote_start_transaction(event_loop):
-    # 获取桩充电之前的状态
-    status = await waitConnectorStatus(1, "Preparing")
-    assert status == "Preparing"
-
-    # 远程启动充电
-    clearTriggerMessage()
-    with open("schema/RemoteStartTransaction.json", 'r') as f:
-        data = json.load(f)
-    response = await service.remoteStartTransaction(event_loop, id_tag=data.get('idTag'),
-                                                    connector_id=data.get('connectorId'),
-                                                    charging_profile=data.get('chargingProfile'))
-    assert response[0].status == RegistrationStatus.accepted
-
-
-    # 等待充电桩鉴权
-    flag, msg = await waitRequest("authorize")
-    assert flag == True
-    logging.info(msg)
-    assert msg['id_tag'] == data.get('idTag')
-
-    # # 等待本地开始充电
-    # flag, msg = await waitRequest("start_transaction")
-    # assert flag == True
-    # logging.info(msg)
-
-    # 获取桩充电之后的状态
-    status = await waitConnectorStatus(1, "Charging", 100)
-    assert status == "Charging"
-
-    clearTriggerMessage()
-    # 结束远程充电
-    response = await service.remoteStopTransaction(event_loop, data['chargingProfile']['transactionId'])
-    assert response[0].status == RegistrationStatus.accepted
-
-    # 等待本地开始充电
-    flag, msg = await waitRequest("stop_transaction")
-    logging.info(msg)
-    assert flag == True
-    assert msg['reason'] == "Remote"
-
-    # 获取桩结束充电之后的状态
-    status = await waitConnectorStatus(1, "Finishing")
-    assert status == "Finishing"
